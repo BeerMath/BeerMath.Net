fdf4206b99de14bacb19bc4d857d2dfae5ea56bf
Matt Cooper <vtbassmatt@gmail.com>
2017-04-19 19:26:28 -0400

    normalize IBU

--- src/BeerMathLib/Bitterness/Ibu.cs
+++ src/BeerMathLib/Bitterness/Ibu.cs
@@ -2,9 +2,6 @@ namespace BeerMath
 {
     public class Ibu : BeerValue
     {
-        // private constructor so consumers cannot create
-        private Ibu() { }
-
         public Ibu(decimal raw)
         {
             Value = raw;
